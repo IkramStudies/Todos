6bf52f458e5a6760ce1071bcea677002fa0936d5
Ikram ul Haq <ikramwani51@gmail.com>
2026-06-18 15:44:47 +0530

    Edit Functionality Implemeted, did not know that in order to edit a specific dom element, we have to store it's id, and compare it

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -3,40 +3,79 @@ import { useState } from "react";
 const Todos = () => {
   const [input, setInput] = useState("");
   const [tasks, setTasks] = useState([]);
-  const addTask = () => {
-    setTasks([{ ...tasks, title: input, id: Date.now(), completed: false }]);
+  const [deletedTasks, setTask] = useState([]);
+  const [editMode, setMode] = useState(false);
+  const [editText, setText] = useState("");
+  const [editId, setId] = useState("");
+  const addTask = (e) => {
+    e.preventDefault();
+    setTasks([...tasks, { title: input, id: Date.now(), completed: false }]);
+    setInput("");
+  };
+  const saveTask = (id) => {
+    setTasks(
+      tasks.map((val) => (val.id == id ? { ...val, title: editText } : val)),
+    );
+    setMode(false);
+  };
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((val) => val.id != id));
   };
   return (
     <div className="">
-      <div className="flex justify-center ">
-        <input
-          type="text "
-          className="h-6 w-40 border rounded-sm"
-          onChange={(e) => setInput(e.target.value)}
-        />
-        <button
-          className="text-[12px] ml-2 border p-1 rounded-sm"
-          onClick={addTask}
-        >
-          Add Task
-        </button>
-      </div>
-      <div className="flex max-sm:flex-wrap gap-40 mt-10">
-        <div>Completed</div>
-        <div>
-          In Queue
-          <ul className="flex gap-6 pt-6">
-            {tasks.map((val) => (
-              <>
-                <li>{val.title}</li>
-                <button className="border p-2 rounded-sm ">Edit Task</button>
-                <button className="border p-2 rounded-sm">Delete Task</button>
-                <button className="border p-2 rounded-sm">Completed </button>
-              </>
-            ))}
-          </ul>
-        </div>
-        <div>Deleted</div>
+      <input
+        type="text "
+        className="h-6 w-40 border rounded-sm"
+        onChange={(e) => setInput(e.target.value)}
+        value={input}
+      />
+      <button
+        className="text-[12px] ml-2 border p-1 rounded-sm"
+        onClick={addTask}
+      >
+        Add Task
+      </button>
+      <ul className="pt-6">
+        {tasks.map((val) =>
+          editMode && val.id == editId ? (
+            <>
+              <div>
+                <input
+                  type="text"
+                  onChange={(e) => setText(e.target.value)}
+                  value={editText}
+                />
+                <button className="border" onClick={() => saveTask(val.id)}>
+                  Save Task
+                </button>
+              </div>
+            </>
+          ) : (
+            <>
+              <li className="mt-3">{val.title}</li>
+              <button
+                className="border p-2 rounded-sm "
+                onClick={() => {
+                  setMode(true);
+                  setId(val.id);
+                }}
+              >
+                Edit Task
+              </button>
+              <button
+                className="border p-2 rounded-sm"
+                onClick={() => deleteTask(val.id)}
+              >
+                Delete Task
+              </button>
+              <button className="border p-2 rounded-sm">Completed </button>
+            </>
+          ),
+        )}
+      </ul>
+      <div className="mt-10">
+        Deleted
+        <ul></ul>
       </div>
     </div>
   );
